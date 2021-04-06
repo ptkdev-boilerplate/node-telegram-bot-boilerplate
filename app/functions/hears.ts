9f699b6f5b16763786bb38a6aef22ed64515dfe5
Patryk Rzucidlo (@PTKDev) <ptkdev@gmail.com>
2021-04-06 10:57:18 +0200

    [Fix] Misprint

--- app/functions/hears.ts
+++ app/functions/hears.ts
@@ -9,6 +9,12 @@
  */
 import bot from "@app/functions/telegraf";
 
+/**
+ * hears: any taxt
+ * =====================
+ * Listen any text user write
+ *
+ */
 const text = async (): Promise<void> => {
 	bot.on("text", (ctx) => {
 		ctx.telegram.sendMessage(ctx.message.chat.id, `Your text --> ${ctx.update.message.text}`);
