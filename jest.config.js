b617aa438c3737f8842d09178e1c7a1129783b63
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2021-11-21 13:58:46 +0100

    [Fix] Jest

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,7 @@ module.exports = {
 	testEnvironment: "node",
 	globals: {
 		"ts-jest": {
-			"tsconfig": "tsconfig.json",
-			"compiler": "ttypescript"
-		}
+			tsconfig: "tsconfig.json",
+		},
 	},
 };
